7466b414f99bf5088b6dd266196305676ff164fd
Zac Crawford <zwcrawford@gmail.com>
2019-05-24 21:06:01 -0500

    fetching api data to console

--- js_6/main.js
+++ js_6/main.js
@@ -4,6 +4,14 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 const cities = [];
 
 // 2. Browsers can use fetch instead of JSON.
-// 3. Fetch will return a promise, so set it to a variable
-const prom = fetch(endpoint);
-console.log(prom)
+// 3. Fetch itself will return a promise, so set it to a variable
+fetch(endpoint)
+  // How we work with promises. Convert the raw data to JSON.
+  // In the second .then, we cannot assign "data" to "cities" because
+  // we have "cities" set as a const above. We can set it to let
+  // or just do what we did below. This push with the spread will add
+  // one city at a time to cities.
+  .then(blob => blob.json())
+  .then(data => cities.push(...data))
+// You can see that it is listed as a Promise in the console.
+console.log(cities);
