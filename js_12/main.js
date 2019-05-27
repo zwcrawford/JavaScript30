cfc55dfb498067030cf852df7c29afc2274db8da
Zac Crawford <zwcrawford@gmail.com>
2019-05-27 15:46:48 -0500

    added comments

--- js_12/main.js
+++ js_12/main.js
@@ -3,9 +3,11 @@ const secretCode = 'zac';
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
+  // start searching from the back of the array.
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join('').includes(secretCode)) {
     console.log('DING DING!');
+    // Adds random rainbows and unicorns to page, lol.
     cornify_add();
   }
   console.log(pressed);
