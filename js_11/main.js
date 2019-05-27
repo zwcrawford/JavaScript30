08392b31fd8d152425364b4cb3716d3db33f0437
Zac Crawford <zwcrawford@gmail.com>
2019-05-27 12:17:59 -0500

    video player and its elements are working

--- js_11/main.js
+++ js_11/main.js
@@ -8,7 +8,7 @@ const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const ranges = player.querySelectorAll(".player__slider");
 
 // 2. Build functions
 
@@ -48,9 +48,26 @@ function updateButton() {
   toggle.textContent = icon;
   //console.log("Update the button");
 }
-// This is working with the data-skip attribute on the button.
+// This is working with the data-skip attribute set on the button.
 function skip() {
-  console.log("skipping");
+  //console.log("skipping");
+  //console.log(this.dataset.skip); // Gives you the data
+  // This is a string so use parseFloat
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  //console.log(this.value);
+}
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  //console.log(e)
 }
 // 3. Attach event listeners
 
@@ -59,6 +76,11 @@ function skip() {
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+
+//Progress slider
+video.addEventListener("timeupdate", handleProgress);
+
+
 // Doing the same for the togglePlay control.
 toggle.addEventListener("click", togglePlay);
 
@@ -74,3 +96,13 @@ toggle.addEventListener("click", togglePlay);
 */
 // Skip and rewind buttons
 skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
+
+//Lastly, add the full-page button as an extra challenge.
